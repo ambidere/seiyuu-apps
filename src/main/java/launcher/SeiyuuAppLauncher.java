43522ba8a3a96c39b2a990879fb3a745f47d170f
ambidere <xanthiandere@gmail.com>
2016-12-11 12:06:35 +0800

    Added simple hello world to test gradle configuration

---
+++ src/main/java/launcher/SeiyuuAppLauncher.java
@@ -0,0 +1,14 @@
+package launcher;
+
+/**
+ * File launcher for Seiyuu related applications.
+ *
+ * Created by ambidere on 12/11/16.
+ */
+public class SeiyuuAppLauncher {
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
